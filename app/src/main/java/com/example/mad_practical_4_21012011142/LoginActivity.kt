cbd2ef9f9ec7af1569bbe24a855171bf58964833
pmsolanki23 <spruthvi863@gmail.com>
2023-10-01 13:05:37 +0530

    completed Practical-4

--- app/src/main/java/com/example/mad_practical_4_21012011142/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011142/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.mad_practical_4_21012011142
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val Register: Button = findViewById(R.id.Signup)
+
+        Register.setOnClickListener{
+            Intent(this@LoginActivity,RegisterActivity::class.java).also { startActivity(it) }
+        }
     }
 }
--- app/src/main/java/com/example/mad_practical_4_21012011142/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_21012011142/RegistrationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mad_practical_4_21012011142
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class RegistrationActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val Login: Button = findViewById(R.id.button)
+
+        Login.setOnClickListener {
+            Intent(this@RegisterActivity, LoginActivity::class.java).also { startActivity(it) }
+        }
+
     }
 }
